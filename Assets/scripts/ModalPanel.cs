b0afb1fc70a2cc2353c5220385f94960fd41d29f
mclower <mclower@g.clemson.edu>
2019-03-08 18:02:28 -0500

    added modal panels for premium juice flavor descriptions

---
+++ Assets/scripts/ModalPanel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ModalPanel : MonoBehaviour {
+
+    public GameObject modalPanelObject;
+    public Text description;
+    public Button go_back;
+
+    public static ModalPanel modalPanel;
+
+    public static ModalPanel Instance()
+    {
+        if (!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+            if (!modalPanel)
+                Debug.LogError("There needs to be one active ModalPanel script on a GameObject in your scene.");
+        }
+
+        return modalPanel;
+    }
+    
+    public void Choice(string description, UnityAction cancelEvent)
+    {
+        modalPanelObject.SetActive(true);
+
+        go_back.onClick.RemoveAllListeners();
+        go_back.onClick.AddListener(cancelEvent);
+        go_back.onClick.AddListener(ClosePanel);
+
+        this.description.text = description;
+        
+        go_back.gameObject.SetActive(true);
+    }
+
+    void ClosePanel()
+    {
+        modalPanelObject.SetActive(false);
+    }
+}
